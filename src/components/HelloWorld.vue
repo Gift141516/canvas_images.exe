a25939b93a345f123c58dbdeb286a57a3bb3d9d2
gift <1415167871@qq.com>
2022-07-26 17:22:21 +0800

    首次上传

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="word"></div>
+</template>
+
+<style scoped>
+.word{
+  height: 100%;
+  width: 100%;
+}
+</style>
